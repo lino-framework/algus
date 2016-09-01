89049bc354dcd1c8d2c993c13bf53e6852dbda0e
Luc Saffre <luc.saffre@gmail.com>
2016-09-01 20:14:11 +0300

    http://luc.lino-framework.org/blog/2016/0901.html

--- lino_algus/__init__.py
+++ lino_algus/__init__.py
@@ -13,9 +13,11 @@
 
 """
 
-from past.builtins import execfile
 from os.path import join, dirname
-execfile(join(dirname(__file__), 'setup_info.py'))
+fn = join(dirname(__file__), 'setup_info.py')
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
+# above line is equivalent to "execfile(filename)", except that it
+# works also in Python 3
 
 __version__ = SETUP_INFO.get('version')
 
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-from past.builtins import execfile
-execfile('lino_algus/setup_info.py')
+fn = 'lino_algus/setup_info.py'
+exec(compile(open(fn, "rb").read(), fn, 'exec'))
+
 if __name__ == '__main__':
     setup(**SETUP_INFO)
