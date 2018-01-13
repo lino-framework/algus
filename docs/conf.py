6b14206caf31a6105d0e4f8d98e8aef5aa4cf735
Luc Saffre <luc.saffre@gmail.com>
2018-01-13 07:59:03 +0200

    http://luc.lino-framework.org/blog/2018/0113.html

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ for n in 'atelier lino lino_xl'.split():
 
 # General information about the project.
 project = "Lino Algus"
-copyright = u'2016, Luc Saffre'
+copyright = '2016-2018 Luc Saffre'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
