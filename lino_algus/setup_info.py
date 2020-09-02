f0d2d8eb5c17ae90170b2bb34314e1fbd465c5cd
Luc Saffre <luc.saffre@gmail.com>
2020-09-02 04:41:43 +0300

    http://luc.lino-framework.org/blog/2020/0901.html

--- lino_algus/setup_info.py
+++ lino_algus/setup_info.py
@@ -45,7 +45,7 @@ in variable names or text which is not related to the projet name.
 
 SETUP_INFO.update(classifiers="""
 Programming Language :: Python
-Programming Language :: Python :: 2
+Programming Language :: Python :: 3
 Development Status :: 4 - Beta
 Environment :: Web Environment
 Framework :: Django
